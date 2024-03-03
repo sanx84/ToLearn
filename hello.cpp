1ca2eaecbee34a942c83918e93f7d481fb5c1a17
Sanjeev Ranjan <ranjan.sanjeev84@gmail.com>
2024-03-03 16:15:30 +0530

    Ticket 1 Changes done by user 2

--- hello.cpp
+++ hello.cpp
@@ -7,6 +7,7 @@ class DemoClass
 public:
 void print();
 void Ticket2();
+void Ticket1();
 };
 
 void DemoClass::print()
@@ -17,8 +18,13 @@ for(int i=0; i<10; ++i)
 }
 }
 
+
 void DemoClass::Ticket2(){
 	std::cout << "Ticket 2 changes" << std::endl;
+}
+void DemoClass::Ticket1()
+{
+	std::cout << "Ticket 1" << std::endl;
 }
 
 int main()
