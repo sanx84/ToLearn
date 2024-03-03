7ee1939e6a478e88180f4a3ca0423c59eb7a4a5f
sanx84 <37654350+sanx84@users.noreply.github.com>
2024-03-03 17:06:10 +0530

    Ticket 1 (#2)
    * Ticket 1 Changes done by user 2
    
    * Ticket 1 Changes
    
    * File name changed

--- hello1.cpp
+++ hello1.cpp
@@ -7,6 +7,7 @@ class DemoClass
 public:
 void print();
 void Ticket2();
+void ticket1();
 };
 
 void DemoClass::print()
@@ -19,6 +20,10 @@ for(int i=0; i<10; ++i)
 
 void DemoClass::Ticket2(){
 	std::cout << "Ticket 2 changes" << std::endl;
+}
+
+void DemoClass::ticket1(){
+	std::cout << "Ticket 1" << std::endl;
 }
 
 int main()
